73f468e7e820f7111f833f9ca8201353c82be71c
Pryangan Chowdhury <pryangan.chowdhury@gmail.com>
2018-07-19 21:56:02 +0530

    Add files via upload

---
+++ Loops/OddNumber.java
@@ -0,0 +1,15 @@
+public class OddNumber
+{
+	public static void main(String args[])
+	{
+		int no=1;
+		while(no<=100)
+		{
+			if(no%2 == 1)
+			{
+				System.out.print(no+" ");
+			}
+			no++;
+		}
+	}
+}
